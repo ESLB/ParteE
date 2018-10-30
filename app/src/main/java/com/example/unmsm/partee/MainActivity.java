af63f2167e8e7054679f84b0f26d4f055e329a31
ESlB <eslevano@hotmail.com>
2018-10-29 19:13:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/unmsm/partee/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.unmsm.partee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+}
